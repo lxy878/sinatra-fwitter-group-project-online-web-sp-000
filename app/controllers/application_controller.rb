781d5b4dbed5cbcd50cb78b22c910c946aaebdfa
Xiaoyi Liu <lxy878@gmail.com>
2020-06-09 22:49:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable  :sessions
+    set :session_secret, 'sometHing_noT_tO_tEll'
+  end
+
+  get '/' do
+    erb :'index'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    erb :'users/create_user'
+  end
 
+  post '/signup' do
+    new_user = User.new(params)
+    if new_user.username.empty? or new_user.email.empty? or !new_user.save
+      redirect '/signup'
+    end
+    redirect '/tweets'
+  end
 
+  get '/login' do
+    if session.has?(:user_id)
+      redirect '/tweets'
+    end
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      'fail login'
+    end
+
+  end
+  get 'logout' do
+
+    redirect '/'
+  end
 end
