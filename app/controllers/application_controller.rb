05265fe046617195837032ac3434e100213f2364
Xiaoyi Liu <lxy878@gmail.com>
2020-06-09 23:12:43 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,19 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
   helpers do
-    def login?
-      session.has_key?(:user_id)
+    def current_dep
+      Department.find(session[:dep_id]) if session[:dep_id]
+    end
+
+    def logged_in?
+        !!current_dep
+    end
+
+    def authentication_required
+        if !logged_in?
+            flash[:authenticate_error] = '* You must log in.'
+            redirect '/'
+        end
     end
   end
 end
