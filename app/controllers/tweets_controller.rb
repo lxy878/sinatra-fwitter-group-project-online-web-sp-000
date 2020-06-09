789af1db032caebaa08a0fd70f3165492094b2e4
Xiaoyi Liu <lxy878@gmail.com>
2020-06-09 23:50:43 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,11 +2,14 @@ class TweetsController < ApplicationController
   before '/tweet*' do
     authentication_required
   end
-  
+
   get '/tweets' do
     @user = current_user
     erb :'tweets/tweets'
   end
 
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
 
 end
