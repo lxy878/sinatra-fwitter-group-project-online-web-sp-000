a81935240960db84d6e980cc88e4892de1fdbd7d
Xiaoyi Liu <lxy878@gmail.com>
2020-06-09 23:40:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :'index'
   end
+  
   helpers do
     def current_user
       User.find(session[:user_id]) if session[:user_id]
@@ -24,48 +25,5 @@ class ApplicationController < Sinatra::Base
     def authentication_required
         redirect '/login' if !logged_in?
     end
-  end
-
-  get '/signup' do
-    if logged_in?
-      redirect '/tweets'
-    end
-    erb :'users/create_user'
-
-  end
-
-  post '/signup' do
-    new_user = User.new(params)
-    if new_user.username.empty? or new_user.email.empty? or !new_user.save
-      redirect '/signup'
-    end
-    session[:user_id] = new_user.id
-    redirect '/tweets'
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    end
-    erb :'users/login'
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:username])
-    if user and user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect '/tweets'
-    else
-      redirect '/login'
-    end
-
-  end
-
-  get '/logout' do
-    if !logged_in?
-      redirect '/'
-    end
-    session.clear
-    redirect '/login'
   end
 end
