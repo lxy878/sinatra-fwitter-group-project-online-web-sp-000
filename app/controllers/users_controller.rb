27ef98374158d07517d8987c0fc2dd8664eb14b8
Xiaoyi Liu <lxy878@gmail.com>
2020-06-09 23:08:09 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    if session.has?(:user_id)
+      
+    end
+    erb :'users/create_user'
+
+  end
+
+  post '/signup' do
+    new_user = User.new(params)
+    if new_user.username.empty? or new_user.email.empty? or !new_user.save
+      redirect '/signup'
+    end
+    session[:user_id] = new_user.id
+    redirect '/tweets'
+  end
+
+  get '/login' do
+    if session.has?(:user_id)
+      redirect '/tweets'
+    end
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      'fail login'
+    end
+
+  end
+
+  get 'logout' do
+    redirect '/'
+  end
 end
